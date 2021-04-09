0f2c53b38ddd11b088332ea03caad7c0da07f605
Patrick <patrick@sendhelp.biz>
2021-04-09 09:40:52 -0700

    Update Head

--- pages/_app.js
+++ pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
+          <title>Patrick Thomas Design</title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
